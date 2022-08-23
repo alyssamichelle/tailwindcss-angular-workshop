7b5e7837615ae5a9c1936065b575b8d63e592800
Alyssa Nicoll <alyssaamnell@gmail.com>
2022-08-23 17:31:17 -0500

    some more rearranging

---
+++ 3.1_animated-full--/src/app/filler-content/filler-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'filler-content',
+  templateUrl: './filler-content.component.html',
+  styleUrls: ['./filler-content.component.css']
+})
+export class FillerContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
