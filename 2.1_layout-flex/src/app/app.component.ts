fbf022d641017eabb70232c974a5b858aa68e68e
Alyssa Nicoll <alyssaamnell@gmail.com>
2022-08-29 10:37:25 -0600

    wrap up layout flex demo

--- 2.1_layout-flex/src/app/app.component.ts
+++ 2.1_layout-flex/src/app/app.component.ts
@@ -9,8 +9,15 @@ export class AppComponent implements OnInit {
   cards = ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''];
 
   generateRandomColor() {
-    return "#" + ("000000" + Math.floor(Math.random() * 16777216).toString(16)).substr(-6);
+    return "#" + ("000000" + this.random(16777216).toString(16)).substr(-6);
+  }
 
+  generateRandomHeight() {
+    return `${this.random(150) - 75}px`;
+  }
+
+  random(max: number) {
+    return Math.floor(Math.random() * max);
   }
 
   protected assignRandomColor() {
